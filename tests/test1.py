9d7f074fe75f34d1e50cdb56a260a3bc210d3e8f
Nikolay Korolev <crafterkolyan@mail.ru>
2018-04-30 11:09:15 +0300

    Added tests for subtask 1

--- tests/test1.py
+++ tests/test1.py
@@ -1,2 +1,39 @@
 if True:
+    # true
+    pass
+
+if False:
+    # false
+    pass
+
+if 1 < 2:
+    # true
+    pass
+
+if -7 > -8:
+    # true
+    pass
+
+if 5 == 5:
+    # true
+    pass
+
+if 6 == -6:
+    # false
+    pass
+
+if 2147483647 <= 2147483647:
+    # true
+    pass
+
+if -2147483648 >= 2147483647:
+    # false
+    pass
+
+if 228 != -2147483648:
+    # true
+    pass
+
+if 2147483647 >= 322:
+    # true
     pass
--- tests/test2.py
+++ tests/test2.py
@@ -1,2 +1,5 @@
+if True:
+    pass
+
 if False:
     pass
