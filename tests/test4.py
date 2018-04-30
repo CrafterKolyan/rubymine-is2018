e5bfc82acc164641c81fa9e80eca1118f586adfd
Nikolay Korolev <crafterkolyan@mail.ru>
2018-04-30 20:02:52 +0300

    Added more unobvious tests with modulos

--- tests/test4.py
+++ tests/test4.py
@@ -15,26 +15,65 @@ elif -1 < 2 > 1:
     # true
     pass
 
-if 1 // 0:
+if 5 % 3 == 2:
+    # true
+    pass
+if -5 % 3 == 1:
+    # true
+    pass
+if 5 % -3 == -1:
+    # true
+    pass
+if -5 % -3 == -2:
+    # true
+    pass
+
+if 5 % 3 == -1:
+    # false
+    pass
+if -5 % 3 == -2:
+    # false
+    pass
+if 5 % -3 == 1:
+    # false
+    pass
+if -5 % -3 == 2:
+    # false
+    pass
+
+if 1 // 0 == 15 or -10 % (5 - 10 // 2) + 15 == 17:
     # undefined behaviour. Here should be no RUNTIME
+    # Two warnings: 1. Division by 0, 2. Taking modulo by 0
     pass
 
 if -10 % 0:
     # undefined behaviour. Here should be no RUNTIME
+    # Warning: Taking modulo by 0
     pass
 
 if 1 >> -1 == 2:
     # undefined behaviour. Here should be no RUNTIME
+    # Warning: Shifting by negative number (-1)
     pass
 
-if 1 << -1 == 0:
+if 1 << (17 - 20) == 0:
     # undefined behaviour. Here should be no RUNTIME
+    # Warning: Shifting by negative number (-3)
     pass
 
 if 0 ** 0 == 1:
     # true. No undefined behaviour
     pass
 
-if 0 ** -1 == 1:
+if 0 ** 0 == 0:
+    # false. No undefined behaviour
+    pass
+
+if (0 ** 1) == 0:
+    # true
+    pass
+
+if 0 ** (25 // 6 - 5) == 1:
     # undefined behaviour. Here should be no RUNTIME
+    # Warning: 0 cannot be raised to a negative power (-1)
     pass
