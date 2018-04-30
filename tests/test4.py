870a7e2d0e26ea0e1a5ffbd12d0fce29b0fec8b7
Nikolay Korolev <crafterkolyan@mail.ru>
2018-04-30 15:58:25 +0300

    Added elif test and more unobvious tests

--- tests/test4.py
+++ tests/test4.py
@@ -11,6 +11,9 @@ if -1 < 2 > 1 < 10 > -0 < 123 < 147 > 23 == 23 >= True == 1 <= 2 > False != 1 !=
 if 0 < True > 0 and - 1 + (123 - 44) and False < 1 > 0 >= -1 <= True < (1 ** 123) + 322:
     # true
     pass
+elif -1 < 2 > 1:
+    # true
+    pass
 
 if 1 // 0:
     # undefined behaviour. Here should be no RUNTIME
@@ -25,5 +28,13 @@ if 1 >> -1 == 2:
     pass
 
 if 1 << -1 == 0:
+    # undefined behaviour. Here should be no RUNTIME
+    pass
+
+if 0 ** 0 == 1:
+    # true. No undefined behaviour
+    pass
+
+if 0 ** -1 == 1:
     # undefined behaviour. Here should be no RUNTIME
     pass
